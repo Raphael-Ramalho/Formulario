2b948be097dbba3132a5b7e61181a4b4044cadda
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-23 19:11:37 -0300

    update

--- js/index.js
+++ js/index.js
@@ -14,20 +14,35 @@ const possibleErros = [
 
 const errorMessages = {
     name: {
-        valueMissing: "O campo Nome não pode ficar vazio."
+        valueMissing: "O campo nome não pode ficar vazio."
     },
     email: {
         valueMissing: "O campo email não pode ficar vazio.",
         customError: "O email digitado não é valido."
+    },
+    birth: {
+        valueMissing: "O campo birth date não pode ficar vazio."
+    },
+    cep: {
+        valueMissing: "O campo cep não pode ficar vazio."
+    },
+    city: {
+        valueMissing: "O campo city não pode ficar vazio."
+    },
+    address: {
+        valueMissing: "O campo address não pode ficar vazio."
     }
 }
 
 const erroFunction = (input) => {
     possibleErros.forEach(error => {
+        let mensagem = ""
         if(input.validity[error]){
-            input.dataset.tipo
-            console.log("campo em branco")
+            const inputType = input.dataset.tipo
+            
+            mensagem = errorMessages[inputType][error]
         }
+        input.setCustomValidity(mensagem)
     })
 }
 
